221032c87f3f9c96010281497dc604473c0694e3
Wanderson <wandersondamasceno1@gmail.com>
2025-05-15 13:15:38 -0300

    fixing app build

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import { useState } from 'react'
+// import { useState } from 'react'
 import Hero from './components/hero/Hero'
 function App() {
-  const [searchTerm, setSearchTerm] = useState('')
+  // const [searchTerm, setSearchTerm] = useState('')
 
   return (
     <>
